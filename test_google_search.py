a153cd265b9c9e02c719d24ec19c7841d656c220
Rishat-F <nilluziaf@gmail.com>
2021-08-20 16:02:14 +0300

    Powerful combination POM + pytest

--- test_google_search.py
+++ test_google_search.py
@@ -1,25 +1,15 @@
-from app import Application
+from pages.main_page import MainPage
 
 
-def test_search_something():
-    app = Application()
-    try:
-        app.main_page.open_page()
-        app.main_page.search("python")
-        assert "python" in app.main_page.get_page_source()
-    finally:
-        app.main_page.quit()
+def test_search_something(browser):
+    main_page = MainPage(browser)
+    main_page.open_page()
+    main_page.search("python")
+    assert "python" in main_page.get_page_source()
 
 
-def test_search_nothing():
-    app = Application()
-    try:
-        app.main_page.open_page()
-        app.main_page.search("")
-        assert "https://www.google.com/" == app.main_page.current_url
-    finally:
-        app.main_page.quit()
-
-
-test_search_something()
-test_search_nothing()
+def test_search_nothing(browser):
+    main_page = MainPage(browser)
+    main_page.open_page()
+    main_page.search("")
+    assert "https://www.google.com/" == main_page.current_url

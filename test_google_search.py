21432d90d7da56fef852bf2cf9267969a6195292
Rishat-F <nilluziaf@gmail.com>
2021-08-20 14:18:11 +0300

    Better realization but still bad

--- test_google_search.py
+++ test_google_search.py
@@ -3,22 +3,31 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-try:
-    driver.get("https://www.google.com/")
-    assert "google" in driver.title.lower()
-    search_input = driver.find_element_by_name("q")
-    search_input.clear()
-    search_input.send_keys("python")
-    search_input.send_keys(Keys.ENTER)
-    assert "python" in driver.page_source
+def test_search_something():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    try:
+        driver.get("https://www.google.com/")
+        search_input = driver.find_element_by_name("q")
+        search_input.clear()
+        search_input.send_keys("python")
+        search_input.send_keys(Keys.ENTER)
+        assert "python" in driver.page_source
+    finally:
+        driver.quit()
 
-    driver.back()
 
-    search_input = driver.find_element_by_name("q")
-    search_input.clear()
-    search_input.send_keys("")
-    search_input.send_keys(Keys.ENTER)
-    assert driver.current_url == "https://www.google.com/"
-finally:
-    driver.quit()
+def test_search_nothing():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    try:
+        driver.get("https://www.google.com/")
+        search_input = driver.find_element_by_name("q")
+        search_input.clear()
+        search_input.send_keys("")
+        search_input.send_keys(Keys.ENTER)
+        assert driver.current_url == "https://www.google.com/"
+    finally:
+        driver.quit()
+
+
+test_search_something()
+test_search_nothing()
